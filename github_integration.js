5b669710b9ba7ca6fed5011f9aee667715832b2c
michwii <freezerhm@yahoo.fr>
2016-03-20 23:53:50 +0100

    change

--- github_integration.js
+++ github_integration.js
@@ -6,7 +6,7 @@ var client_secret = "c84dea3db7f306d4a002318b77aed0d5182c9a7c";
 exports.getAuthorizationToken = function(code, callback) {
   request.post({url : "https://github.com/login/oauth/access_token", form : {code: code, client_id: client_id, client_secret : client_secret}, headers : {Accept: "application/json"}}, function(err, response, body){
     body = JSON.parse(body);
-    callback(err, body);
+    callback(err, body.access_token);
   });
 }
 
