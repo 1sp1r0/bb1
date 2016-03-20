c151b8cb30f9d472dbde7a587385625312450309
michwii <freezerhm@yahoo.fr>
2016-03-20 12:32:47 +0100

    Adding slack integration

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var port = process.env.PORT || 1337;
+var stackOverflowIntergration = require("./stack_overflow_integration");
+
 var app = express();
 
 app.get("/slackOverflow", function(req, res){
+  var tag = req.query.tag;
+  stackOverflowIntergration.checkOutNewMessages(tag);
   res.json({success : "ok", message : "job submitted"});
 });
 
