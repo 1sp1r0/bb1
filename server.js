262fcd03c68b4bbf54df58bb02c083c075c9e3ac
michwii <freezerhm@yahoo.fr>
2016-03-20 12:24:23 +0100

    Adding slackOverflow url

--- server.js
+++ server.js
@@ -2,17 +2,8 @@ var express = require("express");
 var port = process.env.PORT || 1337;
 var app = express();
 
-app.get("/", function(req, res){
+app.get("/slackOverflow", function(req, res){
   res.json({success : "ok", message : "job submitted"});
 });
 
 app.listen(port);
-
-/*
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Pourquoi ca marche pas !!\n');
-}).listen(port);
-*/
