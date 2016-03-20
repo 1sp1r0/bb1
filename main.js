0a0330c18b5372438b8921cc5d9e0dc80e4ae6f0
michwii <freezerhm@yahoo.fr>
2016-03-20 12:03:14 +0100

    Adding one route

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ var express = require("express");
 var port = process.env.PORT || 1337;
 var app = express();
 
+app.get("/", function(req, res){
+  res.json({success : "ok", message : "job submitted"});
+});
+
 app.listen(port);
